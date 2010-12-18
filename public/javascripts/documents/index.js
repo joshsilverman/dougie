b509663ebaa4d636e57b9da72c3ddbaeaafb0b4e
Josh Silverman <joshs.silverman@gmail.com>
2010-12-18 01:13:53 -0500

    frontend partial skeleton

--- public/javascripts/documents/index.js
+++ public/javascripts/documents/index.js
@@ -1,65 +1,86 @@
-var fDocumentsIndex = function() {
+var fDocument = function() {
 
     var outline = new fOutline();
     var rightRail = new fRightRail();
 
-    var init = function() {}.call();
+    var init = function() {}.apply(arguments);
+
+    var save = function() {};
 };
 
 var fOutline = function() {
 
     var outlineHandlers = new fOutlineHandlers();
 
-    var init = function() {}.call();
+    var init = function() {}.apply(arguments);
 };
 
 var fOutlineHandlers = function() {
 
     var init = function() {
 
-        //capture iframe click events
+        //capture iframe keystroke events
         var iframe = $$('#cke_contents_editor iframe')[0];
         var iDoc = iframe.contentWindow || iframe.contentDocument;
         iDoc.document.onkeydown = delegateHandler.curry(iDoc);
 
-    }.delay(1); //@todo this creates race condition
+    }.delay(1, arguments); //@todo this creates race condition
 
     var delegateHandler = function(iDoc, event) {
 
-        //override event target - //@todo not quite there but close!
+        //get real target - target in event object is false
+        //@todo this is not quite there - sometimes it returns a ul; also, I
+        //      couldn't overwrite event.target
         if (iDoc.document.selection) {
-            range = iDoc.document.selection.createRange();
-            event.target = range.parentElement();
+            var range = iDoc.document.selection.createRange();
+            var target = range.parentElement();
         }
         else if (iDoc.window.getSelection) {
             var range = iDoc.window.getSelection().getRangeAt(0);
-            event.target = range.commonAncestorContainer.parentNode || iDoc.document;
+            var target = range.commonAncestorContainer.parentNode;
         }
 
         //invoke proper handlers
         switch (event.keyCode) {
             case Event.KEY_TAB:
-                onTab(event);
+                onTab(event, target);
+                break;
+            case Event.KEY_RETURN:
+                onReturn(event, target);
                 break;
             default:
                 break;
         }
     };
 
-    var onTab = function(event) {
+    var onTab = function(event, target) {
         if (event.shiftKey) CKEDITOR.instances.editor.execCommand('outdent');
         else CKEDITOR.instances.editor.execCommand('indent');
-        Event.stop(event);
+    };
+
+    var onReturn = function(event, target) {
+
+        //@todo ajust spec - no need to add attributes until node has content
+        //      is this necessary
+
     };
 
 };
 
 var fRightRail = function() {
 
-    var init = function() {}.call();
+    var init = function() {}.apply(arguments);
 
     var cards = [];
+
+    var createCard = function() {};
+    
+    var updateCard = function() {};
+
+    var activateCard = function() {};
+
+    var deactivateCard = function() {};
 };
 
 
-var documentsIndex = new fDocumentsIndex();
+var doc = new fDocument();
