1476edbcdbf03d251824bf476813ad3b0360fb66
Josh Silverman <joshs.silverman@gmail.com>
2011-01-01 14:40:44 -0500

    mem creation tests in line_test

--- test/unit/line_test.rb
+++ test/unit/line_test.rb
@@ -623,4 +623,52 @@ class LineTest < ActiveSupport::TestCase
     assert(matches == 6)
   end
 
+  #@todo move? mem active/inactive invoked by line model...
+  def test_mem_active_inactive
+
+    #setup
+    Mem.delete_all #@todo shouldn't be necessary...
+    require 'documents_helper'
+    name = 'new document'
+    html = %q[
+              <body>
+
+                <p changed="1" id="1" parent="0" line_id="" active="true">a This is a test - think<br></p>
+
+                <ul>
+
+                  <li changed="1" id="2" parent="0" line_id="" active="true">a the letter 'a' i am using
+                    <ul>
+                      <li changed="1" id="3" parent="2" line_id="" active="true">a just to keep track</li>
+                      <li changed="1" id="4" parent="2" line_id="" active="false">a of things that are saved on the first</li>
+                      <li changed="1" id="5" parent="2" line_id="" active="false">a run through</li>
+                    </ul>
+                  </li>
+
+                  <li changed="1" id="6" parent="0" line_id="" active="true">a where as items that begin with</li>
+
+                </ul>
+
+              </body>
+            ]
+
+    #save
+    document = Document.find_or_create_by_name(name)
+    Line.create(:text => "root", :domid => 0, :document_id => document.id)
+
+    dp = DocumentsHelper::DocumentParser.new(html)
+    Line.preorder_save(dp.doc, document.id)
+
+    #get recently saved
+    document = Document.find_by_name(name)
+    lines_all = Line.find(:all)
+    lines_tree = Line.find(:first,
+                           :include => { :children => { :children => { :children => { :children => :children }}}},
+                           :conditions => {'lines.parent_id' => nil, 'lines.document_id' => document.id})
+    lines = Line.find(:all, :conditions => {'lines.document_id' => document.id})
+
+    #active attribute assertions
+    puts Mem.all.to_yaml
+
+
 end
