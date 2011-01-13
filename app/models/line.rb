48da9ab42d2ad2ee65ea2b4bdcc3548e64f59279
Josh Silverman <joshs.silverman@gmail.com>
2011-01-12 19:46:54 -0500

    update_line now order n

--- app/models/line.rb
+++ app/models/line.rb
@@ -91,32 +91,37 @@ class Line < ActiveRecord::Base
   end
   
   def self.update_line(lines,existing_lines)
-    
+
+    existing_lines_hash = Hash.new
+    existing_lines.each do |e_line|
+      existing_lines_hash[e_line.id] = e_line
+    end
+
     # find all lines with line_id attribute
-    lines.css("li[line_id]").each do |line|
-      
+    lines.css("li").each do |line|
+
       unless id.blank?
-        existing_lines.each do |e_line|
-          
-          #existing line_number equals incoming line number
-          if e_line.id.to_s == line.attr('line_id').to_s
-            
-            #existing line epoch updated_at less than incoming line epoch change time
-            if e_line.updated_at.to_i < line.attr('changed').to_i
-              
-              # debug
-              # p "LINE CHANGED!"
-              # p "ELINE: #{e_line.updated_at.to_i}"
-              # p "LINE: #{line}"
-              # p "TEXT: #{line.text}"
-              
-              # replace existing line text with incoming line text
-              # updated_at time is automatically set by rails
-              e_line.update_attribute(:text,line.text)
-              
-            end
-          end
+
+        if existing_lines_hash[line.attr('line_id').to_i].blank?
+          next
+        else
+          e_line = existing_lines_hash[line.attr('line_id').to_i]
         end
+
+        # existing line epoch updated_at less than incoming line epoch change time
+#        if e_line.updated_at.to_i < line.attr('changed').to_i
+
+          # debug
+          # p "LINE CHANGED!"
+          # p "ELINE: #{e_line.updated_at.to_i}"
+          # p "LINE: #{line}"
+          # p "TEXT: #{line.text}"
+
+          # replace existing line text with incoming line text
+          # updated_at time is automatically set by rails
+          e_line.update_attribute(:text,line.text)
+
+#        end
       end
     end
   end
