31ec8ba33d93c75e659c954540bce73e45c0c3e0
Josh Silverman <joshs.silverman@gmail.com>
2011-01-16 18:47:54 -0500

    tag names unique by user

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,9 @@ class Tag < ActiveRecord::Base
 
   has_many :documents, :dependent => :destroy
   belongs_to :user
-  
+
+  validates_uniqueness_of :name, :scope => :user_id
+
   def self.tags_json(current_user = nil)
     return nil if current_user.blank?
     current_user.tags.includes(:documents)\
