2678699b6ffe68d6ede2e66670163a37edb3a8b7
Josh Silverman <joshs.silverman@gmail.com>
2011-01-17 11:32:41 -0500

    documents/update now passes most work off to model

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,6 +1,4 @@
 class DocumentsController < ApplicationController
-
-  include DocumentsHelper
   
   # Look for existing documents (by name for now)
   # If exists, use this document, otherwise set document html and construct Line objects
@@ -50,66 +48,16 @@ class DocumentsController < ApplicationController
   
   def update
 
-    id = params[:id]
-    html = params[:html]
-    delete_nodes = params[:delete_nodes]
-    new_nodes = params[:new_nodes] == 'true'
-    @document = current_user.documents.find_by_id(id)
-#    @document = Document.includes(:lines).where(:id => id, :user_id => current_user.id).first //@todo combind existing lines query with this one
-    if id.blank? || html.blank? || @document.blank?
-      render :nothing => true, :status => 400
-      return
-    end
-    
-    # pull all existing document line
-    existing_lines = @document.lines
+    # update document
+    @document = Document.update(params, current_user)
 
-    # group transaction; track whether lines deleted
-    deleted_lines = false
-    Line.transaction do
-      # look for root in existing lines
-      root = nil
-      existing_lines.each do |line|
-        if line.domid == "node_0"
-          root = line
-          break
-        end
-      end
-
-      # create root
-      if root.nil?
-        root = Line.create(:document_id => @document.id,:domid => "node_0",:text => "root" )
-      end
-
-      # run update line; store whether anything was changed
-      dp = DocumentParser.new(html)
-      Line.update_line(dp.doc,existing_lines) unless @document.html.blank?
-
-      Line.document_html = html
-      if (new_nodes)
-        Line.preorder_save(dp.doc,@document.id, {'node_0' => root})
-      end
-
-      @document.update_attributes(:html => Line.document_html, :name => params[:name])
-
-      # delete lines/mems (don't use destory_all with dependencies) - half as many queries; track whether deleted
-      deleted_lines = false
-      unless delete_nodes == '[]' || delete_nodes.nil? || delete_nodes == ''
-        deleted_lines = true
-        Line.delete_all(["id IN (?) AND document_id = ?", delete_nodes.split(','), @document.id])
-        Mem.delete_all(["line_id IN (?)", delete_nodes.split(',')]) # belongs in model but I think before_delete would delete mems infividually
-      end
-    end
-
-    # refresh existing lines and create hash
-    if new_nodes || deleted_lines
-      lines = Line.find_all_by_document_id(id)
-    else
-      lines = existing_lines
+    if @document.nil?
+        render :nothing => true, :status => 400
+        return
     end
 
     # render {line.domid: line.id} hash
-    render :json => Hash[*lines.map {|line| [line.domid, line.id]}.flatten]
+    render :json => Hash[*@document.lines.map {|line| [line.domid, line.id]}.flatten]
     
   end
   
--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,7 @@
 class Document < ActiveRecord::Base
 
+  include DocumentsHelper
+
   has_many :lines, :dependent => :destroy
 
   belongs_to :tag
@@ -11,6 +13,70 @@ class Document < ActiveRecord::Base
     if self.name.blank?
       self.name = "#{Time.now.to_i}"
     end
+  end
+
+  def self.update(params, current_user)
+
+    id = params[:id]
+    html = params[:html]
+    delete_nodes = params[:delete_nodes]
+    new_nodes = params[:new_nodes] == 'true'
+    document = current_user.documents.find_by_id(id)
+#    document = Document.includes(:lines).where(:id => id, :user_id => current_user.id).first //@todo combind existing lines query with this one
+
+    if id.blank? || html.blank? || document.blank?
+      return nil
+    end
+
+    # pull all existing document line
+    existing_lines = document.lines
+
+    # group transaction; track whether lines deleted
+    deleted_lines = false
+    Line.transaction do
+      # look for root in existing lines
+      root = nil
+      existing_lines.each do |line|
+        if line.domid == "node_0"
+          root = line
+          break
+        end
+      end
+
+      # create root
+      if root.nil?
+        root = Line.create(:document_id => document.id,:domid => "node_0",:text => "root" )
+      end
+
+      # run update line; store whether anything was changed
+      dp = DocumentParser.new(html)
+      Line.update_line(dp.doc,existing_lines) unless document.html.blank?
+
+      Line.document_html = html
+      if (new_nodes)
+        Line.preorder_save(dp.doc,document.id, {'node_0' => root})
+      end
+
+      document.update_attributes(:html => Line.document_html, :name => params[:name])
+
+      # delete lines/mems (don't use destory_all with dependencies) - half as many queries; track whether deleted
+      deleted_lines = false
+      unless delete_nodes == '[]' || delete_nodes.nil? || delete_nodes == ''
+        deleted_lines = true
+        Line.delete_all(["id IN (?) AND document_id = ?", delete_nodes.split(','), document.id])
+        Mem.delete_all(["line_id IN (?)", delete_nodes.split(',')]) # belongs in model but I think before_delete would delete mems infividually
+      end
+    end
+
+    # refresh existing if necessary
+    if new_nodes || deleted_lines
+      document.lines = Line.find_all_by_document_id(id)
+    else
+      document.lines = existing_lines
+    end
+
+    return document
+    
   end
   
 end
