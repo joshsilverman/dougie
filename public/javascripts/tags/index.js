0ab65a3fe6a87522199153f2f3c3465229409d2e
Josh Silverman <joshs.silverman@gmail.com>
2011-01-24 15:03:22 -0500

    organizer bugfix - create,back,forward,back

--- public/javascripts/tags/index.js
+++ public/javascripts/tags/index.js
@@ -16,7 +16,7 @@ var cDoc = Class.create({
 
         /* check for reload cookie */
         var reload = AppUtilities.Cookies.read('reloadOrganizer') == 'true';
-        AppUtilities.Cookies.erase('reloadOrganizer')
+//        AppUtilities.Cookies.erase('reloadOrganizer')
         if (reload) {
             //self.document.location.reload(true);
             new Ajax.Request('/tags/json', {
