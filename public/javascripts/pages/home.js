ab9903f6545b5b85ea0756e0901b3f7a9a132bff
Josh Silverman <joshs.silverman@gmail.com>
2011-01-27 11:42:32 -0500

    header styling

--- public/javascripts/pages/home.js
+++ public/javascripts/pages/home.js
@@ -2,7 +2,7 @@
 var cDoc = Class.create({
 
     mouseoverTargets: $H({
-       'learn': '<h1><span style=\'color:#009CFF;\'>Learn</span> by taking notes like you normally would</h1><h5>We\'ll turn it into study material for you!</h5>',
+       'learn': '<h1><span style=\'color:#009CFF;\'>Learn</span> by taking notes as usual</h1><h5>We\'ll turn it into study material for you!</h5>',
        'study': '<h1><span style=\'color:#009CFF;\'>Study</span> using flashcards made from your notes</h1><h5>We\'ll track what\'s important to you and how well you know it!</h5>',
        'know': '<h1><span style=\'color:#009CFF;\'>Know</span> all of your material cold</h1><h5>Study what matters at the right time for improved retention in less time!</h5>'
     }),
