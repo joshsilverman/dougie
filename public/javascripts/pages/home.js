35a7d0a7c729869d12a38cee0ca0b7fbd639cad2
Josh Silverman <joshs.silverman@gmail.com>
2011-01-27 00:15:14 -0500

    home formatting changes; home js now in external file

---
+++ public/javascripts/pages/home.js
@@ -0,0 +1,33 @@
+
+var cDoc = Class.create({
+
+    mouseoverTargets: $H({
+       'learn': '<h1><span style=\'color:#009CFF;\'>Learn</span> by taking notes like you normally would</h1><h5>We\'ll turn it into study material for you!</h5>',
+       'study': '<h1><span style=\'color:#009CFF;\'>Study</span> using flashcards made from your notes</h1><h5>We\'ll track what\'s important to you and how well you know it!</h5>',
+       'know': '<h1><span style=\'color:#009CFF;\'>Know</span> all of your material cold</h1><h5>Study what matters at the right time for improved retention in less time!</h5>'
+    }),
+
+    initialize:function() {
+
+        /* splash listeners */
+        this.mouseoverTargets.each(function(keyValue) {
+
+            var src = '/images/home/' + keyValue[0] + '.png'
+            var srcUnderlined = '/images/home/' + keyValue[0] + '_underline.png'
+
+            /* over */
+            $('img_' + keyValue[0]).observe('mouseover', function(event) {
+                event.target.src = srcUnderlined;
+                document.getElementById('headline').innerHTML = keyValue[1];
+            }.bind(this));
+
+            /* out */
+            $('img_' + keyValue[0]).observe('mouseout', function(event) {event.target.src = src;}.bind(this));
+         });
+    }
+});
+
+/* global objects */
+document.observe('dom:loaded', function() {
+    doc = new cDoc;
+});
