1a3eb2c4dda611f46b068d9dde878fc5d9fe58a4
Josh Silverman <joshs.silverman@gmail.com>
2011-02-08 17:14:23 -0500

    new mailer address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,8 +58,8 @@ Dougie::Application.configure do
       :port => 587,
       :domain => "gmail.com",
       :authentication => :login,
-      :user_name => "dougie.prod@gmail.com",
-      :password => "dougie-prod"
+      :user_name => "zendo.noreply@gmail.com",
+      :password => "93guz93man"
   }
   
 end
